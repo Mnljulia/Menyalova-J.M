0a3798e78a38b5ccaf4d21ed38b85b5196fc97a2
Mnljulia <91316963+Mnljulia@users.noreply.github.com>
2021-09-30 17:38:03 +0300

    Delete 1 и 2 программа.py

--- 1 и 2 программа.py
+++
@@ -1,7 +0,0 @@
-Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> print("Курс Основы программирования начался")
-Курс Основы программирования начался
->>> print(16823*12302/3092)
-66932.90620957309
->>> 
