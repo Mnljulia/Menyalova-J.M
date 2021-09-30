4fd5151cdfdadd6b7e019e73b020b8c57f21d909
Mnljulia <91316963+Mnljulia@users.noreply.github.com>
2021-09-30 17:38:51 +0300

    Delete 4 программа.py

--- 4 программа.py
+++
@@ -1,9 +0,0 @@
-# -- coding: utf-8 --
-print("Введите a,b,l,N, переменные <> нулю")
-a=int(input())
-b=int(input())
-l=int(input())
-N=int(input())
-len=l*2+b*(N-2)+a*(N-1)
-print("Длина шнурка должны быть = ",len)
-
